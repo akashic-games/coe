b5c0fdcd2d46e70f8e3cdd9e9f03c7059d7b24fc
yusuke-ogiwara <yusuke_ogiwara@dwango.co.jp>
2019-08-30 13:14:51 +0900

    Support scenes driven by clock mode

--- packages/coe/src/impl/Scene.ts
+++ packages/coe/src/impl/Scene.ts
@@ -3,23 +3,22 @@ import { View } from "../View";
 import { BaseController } from "./BaseController";
 
 export interface SceneParameters<Command, ActionData> extends g.SceneParameterObject {
-	local?: g.LocalTickMode.InterpolateLocal;
-	tickGenerationMode?: g.TickGenerationMode.Manual;
+	local?: g.LocalTickMode.InterpolateLocal | g.LocalTickMode.NonLocal;
 	controller: BaseController<Command, ActionData>;
 }
 
 /**
  * コントローラにより制御される g.Scene 。
  *
- * 本シーンはローカルティック補間モードかつ手動進行として初期化され、
- * すべての g.MessageEvent がフレームワーク側で握りつぶされる点に注意。
- *
- * 本クラスのインスタンス生成時に渡されるパラメータは以下のように破壊的に変更される点に注意。
+ * 本シーンを利用した場合、すべての g.MessageEvent がフレームワーク側で握りつぶされる点に注意。
+ * また、以下のパラメータの初期値が g.Scene と異なる点に注意。
  * * local: g.LocalTickMode.InterpolateLocal
  * * tickGenerationMode: g.TickGenerationMode.Manual
  */
 export class Scene<Command, ActionData> extends g.Scene implements View<Command, ActionData> {
-	commandReceived: g.Trigger<Command>;
+	commandReceived: g.Trigger<Command> = new g.Trigger();
+	private _generateTickManually: boolean = false;
+	private _sentInitialEvents: boolean = false;
 
 	/**
 	 * 本 Scene に紐づく Controller のインスタンス。
@@ -31,12 +30,14 @@ export class Scene<Command, ActionData> extends g.Scene implements View<Command,
 	private onEventFiltered_bound: (pevs: any[][]) => any[][];
 
 	constructor(params: SceneParameters<Command, ActionData>) {
-		params.local = g.LocalTickMode.InterpolateLocal;
-		params.tickGenerationMode = g.TickGenerationMode.Manual;
-		super(params);
+		super({
+			local: params.local != null ? params.local : g.LocalTickMode.InterpolateLocal,
+			tickGenerationMode: params.tickGenerationMode != null ? params.tickGenerationMode : g.TickGenerationMode.Manual,
+			...params
+		});
 
+		this._generateTickManually = this.tickGenerationMode === g.TickGenerationMode.Manual;
 		this._controller = params.controller;
-		this.commandReceived = new g.Trigger();
 		this.onEventFiltered_bound = this.onEventFiltered.bind(this);
 		this.message.add(this.onReceivedMessageEvent, this);
 		this.stateChanged.add(this.onStateChanged, this);
@@ -44,6 +45,9 @@ export class Scene<Command, ActionData> extends g.Scene implements View<Command,
 		// TODO: 他の判定方法を検討
 		if (getPermission().advance) {
 			this.update.add(this.fireControllerUpdate, this);
+			if (this._generateTickManually) {
+				this.update.add(this.raiseTickIfMessageEventExists, this);
+			}
 			this.loaded.addOnce(() => {
 				this._controller.loaded.fire();
 			});
@@ -72,8 +76,6 @@ export class Scene<Command, ActionData> extends g.Scene implements View<Command,
 	}
 
 	private onEventFiltered(pevs: any[][]): any[][] {
-		if (!pevs.length) return pevs;
-
 		const filtered: any[][] = [];
 
 		for (let i = 0; i < pevs.length; i++) {
@@ -108,7 +110,28 @@ export class Scene<Command, ActionData> extends g.Scene implements View<Command,
 			}
 		}
 
+		if (!this._generateTickManually) {
+			const messages = this._controller.getBroadcastDataBuffer();
+			if (messages) {
+				filtered.push(...messages.map(event => [0x20, null, null, event]));
+			}
+			if (!this._sentInitialEvents) {
+				// NOTE: 手動進行->自動進行切替時に自動進行の開始時刻が不明となってしまうため、シーンの切替時に timestamp を挿入する
+				filtered.unshift([0x2, null, null, Math.floor(g.game.getCurrentTime())]);
+				this._sentInitialEvents = true;
+			}
+		}
+
 		return filtered;
+	}
+
+	private raiseTickIfMessageEventExists(): void {
+		const messages = this._controller.getBroadcastDataBuffer();
+		if (messages) {
+			const events = messages.map(data => new g.MessageEvent(data));
+			const timestamp = new g.TimestampEvent(Math.floor(g.game.getCurrentTime()), null as any);
+			this.game.raiseTick([timestamp, ...events] as any);
+		}
 	}
 
 	private onReceivedMessageEvent(message: g.MessageEvent): void {
@@ -120,6 +143,7 @@ export class Scene<Command, ActionData> extends g.Scene implements View<Command,
 			this.game.removeEventFilter(this.onEventFiltered_bound);
 		} else if (state === g.SceneState.Active) {
 			this.game.addEventFilter(this.onEventFiltered_bound, true);
+			this._sentInitialEvents = false;
 		}
 	}
 }
